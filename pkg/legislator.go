30e90ee8b6004e8f479e9f6a15de9fc38ac94ee7
Kia Farhang <kfarhang0@gmail.com>
2021-12-26 12:53:00 -0600

    chore: add (working) fields to Legislator struct

--- pkg/legislator.go
+++ pkg/legislator.go
@@ -1,29 +1,29 @@
 package opensecrets
 
 type Legislator struct {
-	FirstElected int `json:"first_elected,string"`
-	// Cid            string `json:"cid"`
-	// FirstLast      string `json:"firstlast"`
-	// LastName       string `json:"lastname"`
-	// Party          string `json:"party"`
-	// Office         string `json:"office"`
-	// Gender         string `json:"gender"`
-	// ExitCode       int    `json:"exit_code,string"`
-	// Comments       string `json:"comments"`
-	// Phone          string `json:"phone"`
-	// Fax            string `json:"fax"`
-	// Website        string `json:"website"`
-	// Webform        string `json:"webform"`
-	// CongressOffice string `json:"congress_office"`
-	// BioguideId     string `json:"bioguide_id"`
-	// VoteSmartId    int    `json:"votesmart_id,string"`
-	// FECCandId      string `json:"feccandid"`
-	// TwitterId      string `json:"twitter_id"`
-	// YouTubeURL     string `json:"youtube_url"`
-	// FacebookId     int    `json:"facebook_id,string"`
+	FirstElected   int    `json:"first_elected,string"`
+	Cid            string `json:"cid"`
+	FirstLast      string `json:"firstlast"`
+	LastName       string `json:"lastname"`
+	Party          string `json:"party"`
+	Office         string `json:"office"`
+	Gender         string `json:"gender"`
+	ExitCode       int    `json:"exit_code,string"`
+	Comments       string `json:"comments"`
+	Phone          string `json:"phone"`
+	Fax            string `json:"fax"`
+	Website        string `json:"website"`
+	Webform        string `json:"webform"`
+	CongressOffice string `json:"congress_office"`
+	BioguideId     string `json:"bioguide_id"`
+	VoteSmartId    string `json:"votesmart_id"`
+	FECCandId      string `json:"feccandid"`
+	TwitterId      string `json:"twitter_id"`
+	YouTubeURL     string `json:"youtube_url"`
+	FacebookId     string `json:"facebook_id"`
 	// TODO: Could make this a better type
 	// https://stackoverflow.com/questions/45303326/how-to-parse-non-standard-time-format-from-json
-	// Birthdate string `json:"birthdate"`
+	Birthdate string `json:"birthdate"`
 }
 
 type legislatorWrapper struct {
