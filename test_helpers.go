86816dc3a0dea45107ebf2bcdc7fc7cb47bfcb82
Kia Farhang <kia.farhang@datastax.com>
2022-01-07 16:54:22 -0600

    refactor: use t.Errorf over t.Fatalf in helper functions

--- test_helpers.go
+++ test_helpers.go
@@ -5,21 +5,21 @@ import "testing"
 func assertErrorExists(err error, t *testing.T) {
 	t.Helper()
 	if err == nil {
-		t.Fatalf("Wanted error but got nil")
+		t.Error("Wanted error but got nil")
 	}
 }
 
 func assertNoError(err error, t *testing.T) {
 	t.Helper()
 	if err != nil {
-		t.Fatalf("Wanted no error but got %s", err.Error())
+		t.Errorf("Wanted no error but got %s", err.Error())
 	}
 }
 
 func assertErrorMessage(err error, expectedMessage string, t *testing.T) {
 	t.Helper()
 	if err.Error() != expectedMessage {
-		t.Fatalf("Got error message %s but wanted %s", err.Error(), expectedMessage)
+		t.Errorf("Got error message %s but wanted %s", err.Error(), expectedMessage)
 	}
 
 }
@@ -27,20 +27,20 @@ func assertErrorMessage(err error, expectedMessage string, t *testing.T) {
 func assertStringMatches(got, wanted string, t *testing.T) {
 	t.Helper()
 	if got != wanted {
-		t.Fatalf("Got string %s wanted %s", got, wanted)
+		t.Errorf("Got string %s wanted %s", got, wanted)
 	}
 }
 
 func assertIntMatches(got, wanted int, t *testing.T) {
 	t.Helper()
 	if got != wanted {
-		t.Fatalf("Got int %d wanted %d", got, wanted)
+		t.Errorf("Got int %d wanted %d", got, wanted)
 	}
 }
 
 func assertSliceLength(got, wanted int, t *testing.T) {
 	t.Helper()
 	if got != wanted {
-		t.Fatalf("Got slice length %d wanted %d", got, wanted)
+		t.Errorf("Got slice length %d wanted %d", got, wanted)
 	}
 }
