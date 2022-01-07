21fc1aa3f7cca45393ad0c1e2661aa3687489f90
Kia Farhang <kia.farhang@datastax.com>
2022-01-07 16:56:57 -0600

    feature: add function to parse candidate summary response

--- candidate_summary.go
+++ candidate_summary.go
@@ -1,21 +1,21 @@
 package opensecrets
 
 type CandidateSummary struct {
-	CandidateName string `json:"cand_name"`
-	Cid           string `json:"cid"`
-	Cycle         int    `json:"cycle,string"`
-	State         string `json:"state"`
-	Party         string `json:"party"`
-	Chamber       string `json:"chamber"`
-	FirstElected  int    `json:"first_elected,string"`
-	NextElection  int    `json:"next_election,string"`
-	Total         int    `json:"total,string"`
-	Spent         int    `json:"spent,string"`
-	CashOnHand    int    `json:"cash_on_hand,string"`
-	Debt          int    `json:"debt,string"`
-	Origin        string `json:"origin"`
-	Source        string `json:"source"`
-	LastUpdated   string `json:"last_updated"`
+	CandidateName string  `json:"cand_name"`
+	Cid           string  `json:"cid"`
+	Cycle         int     `json:"cycle,string"`
+	State         string  `json:"state"`
+	Party         string  `json:"party"`
+	Chamber       string  `json:"chamber"`
+	FirstElected  int     `json:"first_elected,string"`
+	NextElection  int     `json:"next_election,string"`
+	Total         float64 `json:"total,string"`
+	Spent         float64 `json:"spent,string"`
+	CashOnHand    float64 `json:"cash_on_hand,string"`
+	Debt          float64 `json:"debt,string"`
+	Origin        string  `json:"origin"`
+	Source        string  `json:"source"`
+	LastUpdated   string  `json:"last_updated"`
 }
 
 type candidateSummaryWrapper struct {
@@ -26,6 +26,6 @@ type candidateSummaryResponse struct {
 	Wrapper candidateSummaryWrapper `json:"summary"`
 }
 
-type candidateResponseWrapper struct {
+type candidateSummaryResponseWrapper struct {
 	Response candidateSummaryResponse `json:"response"`
 }
